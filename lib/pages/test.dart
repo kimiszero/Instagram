17937e7991befb7947e94b5cc2d752ca7d1c8d72
kimiszero <zerokim0215@gmail.com>
2021-06-02 23:20:53 +0900

    mypage

---
+++ lib/pages/test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TestE extends StatelessWidget {
+  const TestE({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('test'),
+    );
+  }
+}
